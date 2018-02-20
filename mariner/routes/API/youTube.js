cf6c3c7ebc427d3b85650a2eb5a2954ff992811b
Joe Jackson <ph8tel@gmail.com>
2018-02-20 12:02:56 -0800

    changed start script for heroku

--- mariner/routes/API/youTube.js
+++ mariner/routes/API/youTube.js
@@ -5,9 +5,9 @@ const DATA = require('./sampleData');
 router.get('/getVideos', function(req, res, next) {
     console.log('target hit');
     //   res.render('', { title: 'Express' });
+
 });
 router.get('/getComments', (req, res, next) => {
-    console.log('comments hit', DATA.items.length);
     const comments = DATA.items.map(comment =>
         comment.snippet.textDisplay)
     res.json(comments)
