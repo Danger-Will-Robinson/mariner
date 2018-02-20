702d5793ced9e55e652b9e9f7b1c84c796c76495
Joe Jackson <ph8tel@gmail.com>
2018-02-20 12:23:36 -0800

    added images route

--- routes/API/youTube.js
+++ routes/API/youTube.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 const DATA = require('./sampleData');
 /* GET home page. */
-router.get('/getVideos', function(req, res, next) {
+router.get('/getCommenterImages', function(req, res, next) {
     console.log('target hit');
     //   res.render('', { title: 'Express' });
-
+    const images = DATA.items.map(comment => {
+        comment.snippet.authorChannelUrl
+    })
+    res.json(images)
 });
 router.get('/getComments', (req, res, next) => {
     const comments = DATA.items.map(comment =>
         comment.snippet.textDisplay)
     res.json(comments)
-    console.log(comments)
 })
 
 module.exports = router;
