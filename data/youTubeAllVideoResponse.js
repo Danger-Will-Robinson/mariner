c757594ec4c4c54827e47c04bce061d3e09e2cbe
Joe Jackson <ph8tel@gmail.com>
2018-02-23 15:33:28 -0800

    server rebuild for integration testing

---
+++ data/youTubeAllVideoResponse.js
@@ -0,0 +1,231 @@
+const ALLVIDEOS = {
+    "nextPageToken": "CAUQAA",
+    "items": [{
+            "status": {
+                "privacyStatus": "public"
+            },
+            "kind": "youtube#playlist",
+            "contentDetails": {
+                "itemCount": 7
+            },
+            "snippet": {
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/lc2RdTenCXM/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/lc2RdTenCXM/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/lc2RdTenCXM/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "maxres": {
+                        "url": "https://i.ytimg.com/vi/lc2RdTenCXM/maxresdefault.jpg",
+                        "width": 1280,
+                        "height": 720
+                    },
+                    "standard": {
+                        "url": "https://i.ytimg.com/vi/lc2RdTenCXM/sddefault.jpg",
+                        "width": 640,
+                        "height": 480
+                    }
+                },
+                "title": "DR650 Back Roads",
+                "channelId": "UCCooOt2LDAfz-5giM99biUQ",
+                "publishedAt": "2015-11-30T03:56:09.000Z",
+                "channelTitle": "ph8tel",
+                "localized": {
+                    "description": "",
+                    "title": "DR650 Back Roads"
+                },
+                "description": ""
+            },
+            "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/tSvKwnuEVl5F9KGad5FQkK-u3vY\"",
+            "id": "PLdl5gB6TdZMmrr4tMHZqJMiEobFtrep51"
+        },
+        {
+            "status": {
+                "privacyStatus": "public"
+            },
+            "kind": "youtube#playlist",
+            "contentDetails": {
+                "itemCount": 15
+            },
+            "snippet": {
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/30FbLrgQl_Y/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/30FbLrgQl_Y/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/30FbLrgQl_Y/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "maxres": {
+                        "url": "https://i.ytimg.com/vi/30FbLrgQl_Y/maxresdefault.jpg",
+                        "width": 1280,
+                        "height": 720
+                    },
+                    "standard": {
+                        "url": "https://i.ytimg.com/vi/30FbLrgQl_Y/sddefault.jpg",
+                        "width": 640,
+                        "height": 480
+                    }
+                },
+                "title": "DR650 Coastal Rides",
+                "channelId": "UCCooOt2LDAfz-5giM99biUQ",
+                "publishedAt": "2015-11-30T03:55:25.000Z",
+                "channelTitle": "ph8tel",
+                "localized": {
+                    "description": "",
+                    "title": "DR650 Coastal Rides"
+                },
+                "description": ""
+            },
+            "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/uXtne_QUvlx9W9lhx4etBsqwCZ8\"",
+            "id": "PLdl5gB6TdZMlIoGq81dlHY--nPga6KKQm"
+        },
+        {
+            "status": {
+                "privacyStatus": "public"
+            },
+            "kind": "youtube#playlist",
+            "contentDetails": {
+                "itemCount": 3
+            },
+            "snippet": {
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/LEAexG6YkX4/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/LEAexG6YkX4/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/LEAexG6YkX4/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    }
+                },
+                "title": "Snowboarding",
+                "channelId": "UCCooOt2LDAfz-5giM99biUQ",
+                "publishedAt": "2015-11-30T03:53:29.000Z",
+                "channelTitle": "ph8tel",
+                "localized": {
+                    "description": "",
+                    "title": "Snowboarding"
+                },
+                "description": ""
+            },
+            "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/pogTEVDPaAArBZO6JYUNhHPQ9xo\"",
+            "id": "PLdl5gB6TdZMkT0i59NM_y1o6gRQkjc9le"
+        },
+        {
+            "status": {
+                "privacyStatus": "public"
+            },
+            "kind": "youtube#playlist",
+            "contentDetails": {
+                "itemCount": 3
+            },
+            "snippet": {
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/17iBWOBIakg/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/17iBWOBIakg/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/17iBWOBIakg/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    }
+                },
+                "title": "animation",
+                "channelId": "UCCooOt2LDAfz-5giM99biUQ",
+                "publishedAt": "2015-11-30T03:52:53.000Z",
+                "channelTitle": "ph8tel",
+                "localized": {
+                    "description": "",
+                    "title": "animation"
+                },
+                "description": ""
+            },
+            "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/Rii1Z92JOYonCO7LsI5sfbqA2Kg\"",
+            "id": "PLdl5gB6TdZMmSOyT8CtssjXD8AtkcnPEI"
+        },
+        {
+            "status": {
+                "privacyStatus": "public"
+            },
+            "kind": "youtube#playlist",
+            "contentDetails": {
+                "itemCount": 4
+            },
+            "snippet": {
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/PDuoTAazVXE/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/PDuoTAazVXE/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/PDuoTAazVXE/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "standard": {
+                        "url": "https://i.ytimg.com/vi/PDuoTAazVXE/sddefault.jpg",
+                        "width": 640,
+                        "height": 480
+                    }
+                },
+                "title": "Kiteboarding",
+                "channelId": "UCCooOt2LDAfz-5giM99biUQ",
+                "publishedAt": "2015-11-30T03:52:05.000Z",
+                "channelTitle": "ph8tel",
+                "localized": {
+                    "description": "",
+                    "title": "Kiteboarding"
+                },
+                "description": ""
+            },
+            "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/IDSQOm_ceH3DOHDhXpz9BrsIcPs\"",
+            "id": "PLdl5gB6TdZMl9K3_O-7THIupK77ndFHui"
+        }
+    ],
+    "kind": "youtube#playlistListResponse",
+    "etag": "\"_gJQceDMxJ8gP-8T2HLXUoURK8c/l5EFP2wSCye56iKRRdMpeADA1yI\"",
+    "pageInfo": {
+        "resultsPerPage": 5,
+        "totalResults": 12
+    }
+}
+module.exports = ALLVIDEOS;
