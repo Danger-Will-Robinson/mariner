a2f0362863c9bb0cc2656ef5448288875c254981
Joe Jackson <ph8tel@gmail.com>
2018-03-05 12:05:34 -0800

    Testing route in place and rendering sample data

--- micro-services/SA/routes/index.js
+++ micro-services/SA/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+// tsting route
+router.get('/test/:testName', (req, res) => {
+    let testName = req.params.testName;
+    if (testName === 'basic') {
+        res.render('basic')
+    }
+    res.render('all', { user: { name: 'ph8tel', id: 'UCCooOt2LDAfz-5giM99biUQ' } })
+})
 
 module.exports = router;
