17044452b79b113c6dd2182175817a3624eff3c4
Alex Boerschinger <writetoalexander@gmail.com>
2018-03-09 19:24:14 +0000

    increased limit to enable large req.body

--- micro-services/comment-retrieval/routes/comments.js
+++ micro-services/comment-retrieval/routes/comments.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const db = require('../../../database/index')
+const bodyParser = require('body-parser');
+// const app = express()
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser({limit: '50mb'}))
 
 router.post('/', (req, res) => {
-  console.log('receving post from Login ')
-  //console.log('receving post from Login req.body looks like ', req.body);
+  console.log('receiving post from Login ')
+  console.log('receving post from Login req.body looks like ', req.body);
   // req.body.videos.map((video) => {
   //   db.query(//insert videos and corresponding comments into mysql db
   //   (err, response) => {
@@ -19,9 +25,9 @@ router.post('/', (req, res) => {
   // res.status(err.status || 500);
   // res.end()
 
-  req.body[0].videos.map((video, index) => {
-    let urlId = video.snippet.thumbnails.default.url.split('vi/')[1].split('/')[0]
-    db.query(`insert into videos (title, thumbnailURL, user, urlId) values ('${video.snippet.title}', '${video.snippet.thumbnails.default.url}', '${req.body[0].name}', '${urlId}')`, (err, result) => {
+  req.body.videos.map((video, index) => {
+    console.log('inside map')
+    db.query(`insert into videos (title, thumbnailURL, user, contentId) values ('${video.snippet.title}', '${video.snippet.thumbnails.default.url}', '${req.body.name}', '${video.contentDetails.videoId}')`, (err, result) => {
       if (err) {
         console.log(`err at index ${index}, err looks like ${err}`)
       } else {
@@ -29,28 +35,29 @@ router.post('/', (req, res) => {
       }
     })   
   })
-  res.status(500).send()
-  res.end()
+   res.status(200).send()
+   res.end()
   
 })
 
 router.get('/', (req, res) => {
-  //axios.get('http://localhost:3000/api/sample')
-  axios.get('https://getmyyoutubedata.herokuapp.com/api/sample')
+  console.log('get request happening in comments app router.get')
+  axios.get('http://localhost:3000/api/sample')
+  //axios.get('https://getmyyoutubedata.herokuapp.com/api/sample')
   .then((response) => {
-    console.log('response.data from Login ', response.data[0].videos)
+    //console.log('response.data from Login ', response.data[0])
     // let url = response.data[0].videos[0].snippet.thumbnails.default.url
     // let urlId = url.split('vi/')[1].split('/')[0]
     // console.log('url is ', url)
     // console.log('urlId is ', urlId)
-    // axios.post('http://localhost:5001/comments', 
-    //   response.data[0])
-    // .then((res) => {
-    //   console.log('response in post from router.get is ')
-    // })
-    // .catch((err) => {
-    //   console.log('err in post from router.get is ')
-    // })
+    axios.post('http://localhost:5001/comments', response.data[0])
+    .then((res) => {
+      console.log('response in post from router.get is ', res)
+    })
+    .catch((err) => {
+      console.log('err in post from router.get is ', err)
+      
+    })
   })
   .catch((err) => {
     console.log('err in get to Login ');
