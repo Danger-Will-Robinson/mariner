edd8fb993cdeefd508c7ac10c3d60f1943ac050c
Alex Boerschinger <writetoalexander@gmail.com>
2018-03-12 19:00:38 +0000

    setup boilerplate

---
+++ client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		videos: []
+  	}
+  	console.log('this.state looks like ', this.state);
+  }
+
+  render() {
+  	return(
+      <h1>Mariner</h1>   
+  	)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
