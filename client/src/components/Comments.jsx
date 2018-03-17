42914181e92d45c5715e4d81b6a217b3363042a7
Sean Spencer <sean_spencer@me.com>
2018-03-17 14:48:05 -0700

    Added some styling to comments list, added classes to <ul> and <li> tags within Comments component.

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Comments = (props) => (
   <div>
-    <ul>
+    <ul class="comment">
     	{props.comments.map((comment) => {
-    	  return <div key="comment.idcomments"><li>{comment.comment}</li></div>	
+    	  return <div key="comment.idcomments"><li class="comment">{comment.comment}</li></div>	
     	})}
     </ul>
   </div>
