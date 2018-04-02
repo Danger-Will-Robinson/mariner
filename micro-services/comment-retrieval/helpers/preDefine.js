add70590defc0e2154f46b3eae7a2f8746e25f81
Alex Boerschinger <writetoalexander@gmail.com>
2018-04-02 20:42:22 +0100

    test in place for sarcasm

--- micro-services/comment-retrieval/helpers/preDefine.js
+++ micro-services/comment-retrieval/helpers/preDefine.js
@@ -4,7 +4,8 @@ let sentiment = require('sentiment');
 let programWords = {
 	'blew': -3,
 	'blows': -3,
-	'sick': 3
+	'sick': 3,
+	
 }
 
 let preDefine = () => {
--- micro-services/comment-retrieval/test/sentimentAnalysisSpec.js
+++ micro-services/comment-retrieval/test/sentimentAnalysisSpec.js
@@ -5,15 +5,14 @@ describe('deepSentimentAnalysis', () => {
 	let data;
   it ('should detect sarcasm that would otherwise sound like praise', () => {	
   	axios.post('http://localhost:5001/analyze', {
-  		text: 'this blows'
+  		text: 'Great Job. Your work blows.'
   	})
   	.then((response) => {
   		console.log('response in test is ', response.data)
-  		let data = response.data
+  		assert.equal(response.data < -1, true)
   	})
   	.catch((err) => {
   		console.log('err in SA test is ', err)
-  	})
-  	assert.equal(data < 3, true)  	
+  	})  	
   })  
 })
