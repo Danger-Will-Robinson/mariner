62a9a00b1c084c7db07f7120e7bd321c22990b75
Sean Spencer <sean_spencer@me.com>
2018-04-02 22:13:27 -0700

    /analysis end point will pull a single comment out of req.body and send back a JSON object containing a breakdown of the analysis from retext-sentiment.

--- controllers/analysisController.js
+++ controllers/analysisController.js
@@ -12,6 +12,27 @@ const processor = unified()
 // module. It will handle a large # of comments.
 
 exports.analyzeComments = (req, res) => {
-    console.log('Analysis endpoint hit with POST');
-    res.end();
+    // console.log('Analysis endpoint hit with POST');
+
+    // Retrieve comment out of req.body:
+    const comment = req.body.comment;
+
+    console.log(req.body);
+    // Create sentence/word tree:
+    const tree = processor.parse(comment);
+    // process.run is needed to produce the "valance" of each word:
+    processor.run(tree, comment);
+    // Print out tree with analysis of each word:
+    // console.log(inspect(tree));
+
+    // Pull out "Data" object from tree:
+    const { data } = tree;
+
+    // Print out valence: (Positive, Neutral, Negative)
+    console.log(data.valence);
+    // Print out polarity: (Score from -10 to 10):
+    console.log(data.polarity);
+
+    // Send back object containing full analysis:
+    res.end(JSON.stringify(tree));
 }
