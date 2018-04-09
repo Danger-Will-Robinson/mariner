55dc5800022d778f797c5ced36cb256714b0c79e
Sean Spencer <sean_spencer@me.com>
2018-04-09 14:38:42 -0700

    Added templates for backdrop component with some basic CSS properties. Modal component also templated.

---
+++ client/src/components/Modal/Modal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Modal.css';
+
+import Backdrop from '../Backdrop/Backdrop';
+
+/* 
+1) Modal should render once a user clicks on a comment from anywhere in the Mariner UI.
+2) Modal will show Comment in a centered box on the page. It will also have a backdrop component that will
+  'darken' the background of the main Mariner page.
+3) Response form will also be shown here, along with a "Reply" button.
+*/
+
+const modal = (props) => (
+  <div>
+  </div>
+);
+
+export default modal;
