65d7af612484efb0a9eafa778c1c1af477570cb1
Joe Jackson <ph8tel@gmail.com>
2018-04-13 13:16:38 -0700

    cleaned up app.js formatting

--- micro-services/Login-App/app.js
+++ micro-services/Login-App/app.js
@@ -1,13 +1,13 @@
-const keys = require('./config/keys');
+const keys = require('./config/keys')
 const passport = require('passport')
-const passportSetup = require('./config/passport-setup');
+const passportSetup = require('./config/passport-setup')
 
 const express = require('express')
 const cookieSession = require('cookie-session')
 
 const cookie = process.env.CKE || require('./config/keys').session.cookieKey
 const mongo = process.env.MNG || require('./config/keys').mongodb.dbURI
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 const app = express()
 
 const authRoutes = require('./routes/auth-routes')
@@ -20,7 +20,7 @@ const mongoose = require('mongoose')
 //set static
 app.use(express.static('static'))
     // set view engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // set up session cookies
 app.use(cookieSession({
@@ -29,8 +29,8 @@ app.use(cookieSession({
 }));
 
 // initialize passport
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 // connect to mongodb
@@ -39,18 +39,20 @@ mongoose.connect(keys.mongodb.dbURI, () => {
 });
 
 // set up routes
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-app.use('/api', apiRoutes);
+app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
+app.use('/api', apiRoutes)
 app.use('/testing', reply)
 
 // create home route
 app.get('/', (req, res) => {
-    res.render('home', { user: req.user });
-});
+        res.render('home', { user: req.user })
+    })
+    //route for testing GUI
 app.get('/test', (req, res) => {
     res.render('api-test', { user: req.user })
 })
+
 app.listen(3000, () => {
     console.log('app now listening for requests on port 3000');
 });
