0998175aa77548ff4b61c3668d5c6efc884fb736
Joe Jackson <ph8tel@gmail.com>
2018-04-18 23:22:24 -0700

    refactored mongoose model

--- micro-services/Login-App/models/user-model.js
+++ micro-services/Login-App/models/user-model.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const userSchema = new Schema({
-//     username: String,
-//     googleId: String,
-//     youTubeId: String,
-//     thumbnail: String
-// });
-
-// const User = mongoose.model('user', userSchema);
-var userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     _id: { type: String },
     access_token: String,
     refresh_token: String,
@@ -20,6 +12,6 @@ var userSchema = new mongoose.Schema({
     commentCountByVideoID: {}
 }, { collection: "you-tube-logins" });
 
-// var User = mongoose.model('User', userSchema);
 const User = mongoose.model('user', userSchema)
+
 module.exports = User;
